4b73a2e1a1ad7e219ac013d526b339323a6a9b4d
Emmanuel De Oliveira <38254662+emdeo@users.noreply.github.com>
2019-07-01 15:56:49 +0200

    ajout de ServiceCours.java

--- src/com/absoluteknowledge/service/ServiceCours.java
+++ src/com/absoluteknowledge/service/ServiceCours.java
@@ -18,8 +18,17 @@ import com.absoluteknowledge.model.Cours;
 import com.absoluteknowledge.utils.HibernateUtil;
 
 public class ServiceCours {
-	//Changer pour passer par entity manager
-	//cf https://thoughts-on-java.org/hibernate-best-practices/
+	
+	/**
+	 * Récupérer un cours dans la BDD
+	 * (changer pour passer par entity manager)
+	 * cf https://thoughts-on-java.org/hibernate-best-practices/
+	 * 
+	 * @param id
+	 * @return
+	 * @throws IllegalStateException
+	 * @throws SystemException
+	 */
 	public Cours getCoursById(long id) throws IllegalStateException, SystemException {
 		Session session = null;
 		Transaction transaction = null;
@@ -28,7 +37,9 @@ public class ServiceCours {
 			session = HibernateUtil.getSessionFactory().openSession();
 			transaction = session.beginTransaction();
 			cour2 = (Cours) session.get(Cours.class, id);
-			//C'est pas utile ici, mais quand on fait un save, ca peut forcer la sauvegarde sur la db immediatement
+
+			// C'est pas utile ici, mais quand on fait un save, ca peut forcer la sauvegarde
+			// sur la db immediatement
 //			session.flush();
 //			session.clear();
 		} catch (Exception e) {
@@ -38,28 +49,36 @@ public class ServiceCours {
 			e.printStackTrace();
 		} finally {
 			if (session != null) {
-				//Je ne ferme pas la session parceque dans le main ou je test le service, quand je tente d'acceder a
-				//une relation de type lazyy j'ai une erreur failed to lazily initialize a collection of role: could not initialize proxy - no Session
-				// vu que je ne suis plus dans transacitonal j'ai plus acces a la session 
-				//session.close();
+				// Je ne ferme pas la session parceque dans le main ou je test le service, quand
+				// je tente d'acceder a
+				// une relation de type lazyy j'ai une erreur failed to lazily initialize a
+				// collection of role: could not initialize proxy - no Session
+				// vu que je ne suis plus dans transacitonal j'ai plus acces a la session
+				// session.close();
 			}
 		}
 		return cour2;
 	}
-	//Utilisation de criteriaQuery pour essayer
-	public List<Cours> getAllCours(){
+
+	/**
+	 * Récupérer tous les cours de la BDD
+	 * Utilisation de criteriaQuery pour essayer
+	 * 
+	 * @return
+	 */
+	public List<Cours> getAllCours() {
 		List<Cours> result = new ArrayList<Cours>();
-		Session session = HibernateUtil.getSessionFactory().openSession(); 
+		Session session = HibernateUtil.getSessionFactory().openSession();
 		EntityManager em = session.getEntityManagerFactory().createEntityManager();
-		
-		 final CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
-	        CriteriaQuery<Cours> crit = criteriaBuilder.createQuery(Cours.class);
-	        Root<Cours> root = crit.from(Cours.class);
+
+		final CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+		CriteriaQuery<Cours> crit = criteriaBuilder.createQuery(Cours.class);
+		Root<Cours> root = crit.from(Cours.class);
 //	        crit.where(criteriaBuilder.equal(root.get("user_name"), userName))
 //	            .distinct(true);
-	        CriteriaQuery<Cours> all = crit.select(root);
-	        result = em.createQuery( all ).getResultList();
-	        
+		CriteriaQuery<Cours> all = crit.select(root);
+		result = em.createQuery(all).getResultList();
+
 		return result;
 	}
 //	public Chapitre getOneChapitreByTitle(String title){
